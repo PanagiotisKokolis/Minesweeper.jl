7934359db14ef73310a83f0ef3cd4e872f6cebbe
Dillon Daudert <dillondaudert@MacBook-Pro-5.local>
2023-03-15 19:43:24 -0400

    initial_skeleton: add app state and handle some button basics

--- src/Minesweeper.jl
+++ src/Minesweeper.jl
@@ -1,7 +1,13 @@
 module Minesweeper
 
+using Logging
 using Random
 using StaticArrays
+using SimpleDirectMediaLayer
+using SimpleDirectMediaLayer.LibSDL2
+
+logger = Base.SimpleLogger(stderr, Logging.Debug)
+global_logger(logger)
 
 include("game.jl")
 include("engine.jl")
--- src/engine.jl
+++ src/engine.jl
@@ -7,6 +7,65 @@
 #   2. update game state
 #   3. render game state
 
+struct Button
+    x::Int
+    y::Int
+    w::Int
+    h::Int
+end
+
+abstract type AppState end
+
+struct MainMenuState <: AppState
+    diff_buttons::Vector{Button}
+end
+
+struct ResultState <: AppState end
+struct PlayState <: AppState end
+struct QuitState <: AppState end
+
+function handle_input end
+
+function handle_input(state::AppState)
+
+    event_ref = Ref{SDL_Event}()
+    
+    while Bool(SDL_PollEvent(event_ref))
+        event = event_ref[]
+        type = SDL_EventType(event.type) # cast the Uint to an SDL_EventType
+        # handle inputs (mostly mouse, but some keyboard events like ESC)
+        # we want to change behavior based on app state.
+        result = handle_input(state, Val(type), event)
+
+        if !isnothing(result)
+            return result
+        end 
+
+        println("Breaking early ! Change me when you should.")
+        break
+    end
+    # if we haven't returned a new state to transition to, return the current state 
+    return state
+end
+
+handle_input(state, type, event) = nothing
+handle_input(state, event, type, key) = nothing
+
+# quit from anywhere whenever we receive a QUIT event
+handle_input(::AppState, ::Val{SDL_QUIT}, _) = QuitState()
+
+# handle keyboard events - we need to pull out the scancode of the button to dispatch
+function handle_input(state::AppState, type::Val{SDL_KEYDOWN}, event)
+    scancode = event.key.keysym.scancode
+    # dispatch on state, SDL_KeyboardEvent, SDL_KEYDOWN, which key (scancode)
+    return handle_input(state, event.key, type, Val(scancode))
+end
+
+handle_input(::MainMenuState, ::SDL_KeyboardEvent, ::Val{SDL_KEYDOWN}, ::Val{SDL_SCANCODE_ESCAPE}) = QuitState()
+
+
+
+
 function start_game()
 
     while true
